87e640ab8cbab2b138235a6a3c64241feb9b5371
Boryczko <facebook@alpha-solutions.pl>
2013-06-26 18:31:36 +0200

    added twitter bootstrap
    + few fixes

--- app/view/layout/default/index.php
+++ app/view/layout/default/index.php
@@ -5,11 +5,20 @@
 	<meta name='description' content='' />
 	<meta name='keywords' content='' />
 	<title>PHP on Rage</title>
-	<link rel='stylesheet' media='screen' href='css/style.css'/>		
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="/css/bootstrap.min.css" rel="stylesheet"/>
+	<link href="/css/bootstrap-responsive.min.css" rel="stylesheet"/>
+	<link href="/bootstrap-ie8buttonfix.css" rel="stylesheet"/>	
+	<link href="/bootstrap-ie7buttonfix.css" rel="stylesheet"/>	
+	<!--[if lte IE 8]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->	
 </head>
 <body>
 	
 	<%=content%>
 
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="/js/bootstrap.min.js"></script>
 </body>
 </html>
